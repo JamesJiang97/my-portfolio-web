206f03f67caad6411693ae9401638392ebdddea2
JamesJiang <jiangzhongqing@outlook.com>
2023-04-11 22:28:33 +0900

    add animation in skills

--- src/components/AppSkills.vue
+++ src/components/AppSkills.vue
@@ -10,7 +10,7 @@
     <div class="skills__container container grid">
       <div>
         <!--==================== SKILLS 1 ====================-->
-        <div class="skills__content">
+        <div class="skills__content" >
 
           <div class="skills__header" @click="skillstate[0] = skillstate[0] === 'true' ? 'false' : 'true'">
             <i class="uil uil-server-network skills__icon"></i>
@@ -26,7 +26,7 @@
             </transition>
           </div>
 
-          <div class="skills__list grid" v-show="skillstate[0] === 'true'">
+          <div class="skills__list grid box1" v-show="skillstate[0] === 'true'">
             <div v-for="skill in skills_1" :key="skill.title">
               <div class="skills__titles">
                 <h3 class="skills__name">{{ skill.title }}</h3>
@@ -39,9 +39,9 @@
 
         </div>
         <!--==================== SKILLS 2 ====================-->
-        <div class="skills__content">
+        <div class="skills__content box2">
 
-          <div class="skills__header" @click="skillstate[1] = skillstate[1] === 'true' ? 'false' : 'true'">
+          <div class="skills__header" @click="skillevent2">
             <i class="uil uil-brackets-curly skills__icon"></i>
 
             <div>
@@ -55,7 +55,7 @@
               <i :class="['uil','uil-angle-down','skills__arrow',skillstate[1]]"></i>
             </transition>
           </div>
-          <!-- <transition class="animate__animated" enter-active-class="animate__fadeInDown" leave-active-class="animate__fadeOutUp"> -->
+          <transition class="animate__animated" enter-active-class="animate__fadeInDown" leave-active-class="animate__fadeOutUp">
             <div class="skills__list grid" key="skills_2" v-show="skillstate[1] === 'true'">
               <div v-for="skill in skills_2" :key="skill.title">
                 <div class="skills__titles">
@@ -66,11 +66,11 @@
                 </div>
               </div>
             </div>
-          <!-- </transition> -->
+          </transition>
         </div>
         <!--==================== SKILLS 3 ====================-->
+        <transition class="move3">
         <div class="skills__content">
-
           <div class="skills__header" @click="skillstate[2] = skillstate[2] === 'true' ? 'false' : 'true'">
             <i class="uil uil-cloud-computing skills__icon"></i>
 
@@ -97,6 +97,7 @@
             </div>
           </div>
         </div>
+      </transition>
 
 
 
@@ -171,6 +172,21 @@ export default {
 
     return { skills_1, skills_2, skills_3, skillstate }
   },
+
+  methods:{
+    skillevent2(event){
+      if(event){
+        this.skillstate[1] = this.skillstate[1] === 'true' ? 'false' : 'true'
+      }
+      if (this.skillstate[1] === 'true'){
+        const box2Hight = document.querySelector('.box2').clientHeight
+        document.querySelector('.move3').style.transform = `translateY(${box2Hight}px)`
+      }
+      if (this.skillstate[1] === 'false'){
+        document.querySelector('.move3').style.transform = `translateY(0px)`
+      }
+    }
+  }
   // methods: {
   //   toggleSkills() {
   //     const skillsContent = document.querySelectorAll('.skills__content')
@@ -250,67 +266,23 @@ export default {
   }
 }
 
-.skills__close .skills__list {
-
-  height: 0;
-  overflow: hidden;
-
-}
+// .skills__close .skills__list {
+//   height: 0;
+//   overflow: hidden;
+// }
 
 .true {
   transform: rotate(-180deg);
 }
 
-// .skills__open .skills__arrow {
-//   transform: rotate(-180deg);
-// }
-
-
-.fade-enter-active {
-	-webkit-animation: slide-in-blurred-top 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both;
-  animation: slide-in-blurred-top 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both;
+.move3{
+  transition: transform 0.5s ease-out;
 }
 
- @-webkit-keyframes fade-enter-active {
-  0% {
-    -webkit-transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
-            transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
-    -webkit-transform-origin: 50% 0%;
-            transform-origin: 50% 0%;
-    -webkit-filter: blur(40px);
-            filter: blur(40px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateY(0) scaleY(1) scaleX(1);
-            transform: translateY(0) scaleY(1) scaleX(1);
-    -webkit-transform-origin: 50% 50%;
-            transform-origin: 50% 50%;
-    -webkit-filter: blur(0);
-            filter: blur(0);
-    opacity: 1;
-  }
+.animate__animated.animate__fadeInDown{
+  --animate-delay: 5s;
 }
-@keyframes fade-enter-active {
-  0% {
-    -webkit-transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
-            transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
-    -webkit-transform-origin: 50% 0%;
-            transform-origin: 50% 0%;
-    -webkit-filter: blur(40px);
-            filter: blur(40px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateY(0) scaleY(1) scaleX(1);
-            transform: translateY(0) scaleY(1) scaleX(1);
-    -webkit-transform-origin: 50% 50%;
-            transform-origin: 50% 50%;
-    -webkit-filter: blur(0);
-            filter: blur(0);
-    opacity: 1;
-  }
-}
+
 
 
 </style>
